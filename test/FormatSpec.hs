5bc423551d55d4bfbb21ab93a5be55e40956be62
Sönke Hahn <soenkehahn@gmail.com>
2023-09-25 17:45:37 -0400

    trigger ci

--- test/FormatSpec.hs
+++ test/FormatSpec.hs
@@ -8,7 +8,7 @@ import Test.Mockery.Directory
 
 wrapTest :: IO () -> IO ()
 wrapTest test = do
-  result <- timeout 200000 test
+  result <- timeout 200001 test
   case result of
     Nothing -> error "timeout"
     Just () -> return ()
